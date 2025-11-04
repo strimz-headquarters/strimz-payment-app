f2ac124fe16b22fdc09f57a40301fa285b4d6446
signor1 <emmanuelomemgboji@gmail.com>
2025-11-04 12:22:38 +0100

    feat(biz): update ui

--- app/business/page.tsx
+++ app/business/page.tsx
@@ -3,7 +3,7 @@ import BusinessDashboardHome from "@/components/business/Dashboard";
 export default function BusinessDashboard() {
     return (
         <main className="w-full">
-            {/* Business page content goes here */}
+            {/* Business dashboard */}
             <BusinessDashboardHome />
         </main>
     )
