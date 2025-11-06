05660c3b4ea02a86ec748018cbfe61d5c1d84ebb
signor1 <emmanuelomemgboji@gmail.com>
2025-11-06 17:30:42 +0100

    chore: payment layout

---
+++ app/payment/page.tsx
@@ -0,0 +1,9 @@
+import UserPaymentPage from "@/components/payment";
+
+export default function PaymentPage() {
+    return (
+        <main className="w-full h-full">
+            <UserPaymentPage />
+        </main>
+    )
+}
