9091731bb431bad34f29e74dcaa95ff4d4f38f9e
signor1 <emmanuelomemgboji@gmail.com>
2025-11-07 22:35:16 +0100

    feat: user account page

--- app/user/account/page.tsx
+++ app/user/account/page.tsx
@@ -1,7 +1,9 @@
+import UserAccountSettings from "@/components/user/Account"
+
 export default function AccountPage() {
     return (
         <main className="w-full">
-
+            <UserAccountSettings />
         </main>
     )
 }
